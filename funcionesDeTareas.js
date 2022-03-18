3d44e681eea0d25265c5131ede6774013023fcae
lpnahuel <nahuel.larrea@hotmail.com>
2022-03-18 14:45:05 -0300

    agregue la funcion escribirJSON y la funcion guardarTarea

--- funcionesDeTareas.js
+++ funcionesDeTareas.js
@@ -5,5 +5,17 @@ let archivoTareas = {
     leerArchivo: function () {
         return JSON.parse(fs.readFileSync(this.archivo, 'utf-8'));
     },
+
+    escribirJSON: function(tareas){
+        let auxiliarTareas = JSON.stringify(tareas, null, " ");
+        fs.writeFileSync(this.archivo, auxiliarTareas);
+    },
+
+    guardarTarea: function(tarea){
+        let tareas = this.leerArchivo();
+        tareas.push(tarea);
+        this.escribirJSON(tareas);
+
+    }
 }
 module.exports = archivoTareas;
